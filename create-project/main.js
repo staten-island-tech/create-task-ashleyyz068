5face346421111c60c0d81f93fe509837466a0b2
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-01-17 09:23:42 -0500

    dsafds

--- create-project/main.js
+++ create-project/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+const DOMSelectors = {
+    letter1: document.getElementById("letter1"),
+    letter2: document.getElementById("letter2"),
+    letter3: document.getElementById("letter3"),
+    letter4: document.getElementById("letter4"),
+    letter5: document.getElementById("letter5"),
+}
+
+
+
+
+
+
+
+
+
+
