8b8f80adf2675297f51b6e5ddf76cc827837979d
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-01-29 23:51:51 -0500

    fdadfds

--- create-project/main.js
+++ create-project/main.js
@@ -1,15 +1,10 @@
 import './style.css'
 const DOMSelectors = {
     form: document.getElementById("form"),
-    letter1: document.getElementById("letter1"),
-    letter2: document.getElementById("letter2"),
-    letter3: document.getElementById("letter3"),
-    letter4: document.getElementById("letter4"),
-    letter5: document.getElementById("letter5"),
+    inputs: Array.from(document.querySelectorAll(".input")), 
 }
 
-const words = ['CAUSE',  'BREAD', 'CLEAN', 'CLEAR', 'CLOCK' ]; 
-const wordsMixed = ['USACE', 'DERBA', 'ENACL', 'RECLA', 'KCCLO']
+const words = ['cause',  'bread', 'clean', 'clear', 'clock' ]; 
 
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
