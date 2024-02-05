64efe392a4476322634ece3a4921de1a6a6a2584
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2024-02-04 20:36:25 -0500

    sfad

--- create-project/main.js
+++ create-project/main.js
@@ -1,47 +1,49 @@
-import './style.css'
+import './style.css';
+
 const DOMSelectors = {
     form: document.getElementById("form"),
-    inputEl: document.querySelectorAll(".input"), 
-    inputs: Array.from(document.querySelectorAll(".input")), 
+    inputEl: document.querySelectorAll(".input"),
+    inputs: Array.from(document.querySelectorAll(".input")),
     scrambledWordDisplay: document.getElementById("scrambledWord"),
-    results: document.getElementById("results")
-}
+    results: document.getElementById("results"),
+};
 
-const words = ['cause',  'bread', 'clean', 'clear', 'clock' ]; 
+const words = ['cause', 'bread', 'clean', 'clear', 'clock'];
 
-let ogWord
-let scrambledWord
-let wrongAttempts
+let ogWord;
+let scrambledWord;
+let wrongAttempts;
 
-function newGame (){
-    ogWord = words[Math.floor(Math.random() * words.length)];
-    scrambledWord = ogWord.split('').sort(() => Math.random() - 0.5).join(''); 
+function newGame(wordArray) {
+    ogWord = wordArray[Math.floor(Math.random() * wordArray.length)];
+    scrambledWord = ogWord.split('').sort(() => Math.random() - 0.5).join('');
     DOMSelectors.scrambledWordDisplay.textContent = `Scrambled Word: ${scrambledWord}`;
     wrongAttempts = 0;
-    counter(); 
+    counter();
 }
 
-function Guessing (){
-    const guess =  DOMSelectors.inputs.map(el => el.value).join('');
+function Guessing() {
+    const guess = DOMSelectors.inputs.map(el => el.value).join('');
     if (guess.toLowerCase() === ogWord.toLowerCase()) {
-        DOMSelectors.results.textContent = `Congrats! You guessed the word!`;
-    }else{
-        DOMSelectors.results.textContent = `Wrong! Please guess again!`;
-        wrongAttempts = wrongAttempts +1; 
-        counter(); 
+        DOMSelectors.results.textContent = `Congrats! You guessed the word correctly!`;
+    } else {
+        DOMSelectors.results.textContent = `Wrong! Please try again!`;
+        wrongAttempts = wrongAttempts + 1;
+        counter();
     }
-    DOMSelectors.inputEl.forEach(el=>el.value =""); 
+    DOMSelectors.inputEl.forEach(el => el.value = "");
 }
+
 function counter() {
     document.getElementById("wrongAttempts").textContent = `Wrong Attempts: ${wrongAttempts}`;
 }
 
 DOMSelectors.form.addEventListener("submit", function (event) {
     event.preventDefault();
-    Guessing(); 
+    Guessing();
 });
 
-newGame(); 
+newGame(words);
 
 
 
